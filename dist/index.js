22464ee9098f918a36cb0e3cfe34985d117af4fa
Dohello <jakeguzzardo2010@gmail.com>
2023-04-23 14:49:11 -0400

    typescript configured

---
+++ dist/index.js
@@ -0,0 +1,7 @@
+"use strict";
+let tsExample = 10;
+while (tsExample < 50) {
+    tsExample += 10;
+}
+console.log(tsExample);
+//# sourceMappingURL=index.js.map
---
+++ src/index.ts
@@ -0,0 +1,7 @@
+let tsExample: number = 10
+
+while(tsExample < 50){
+    tsExample += 10
+}
+
+console.log(tsExample)
