a27ac64447c183b7cd5304484e413d54f5abed80
Dohello <jakeguzzardo2010@gmail.com>
2023-04-29 18:41:14 -0400

    idk

--- public/lobbies.js
+++ public/lobbies.js
@@ -2,13 +2,13 @@ let createLoby = document.getElementById('create-form')
 let joinLoby = document.getElementById('join-form')
 let lobyId = -1
 
-createLoby.addEventListener('submit', (e) =>{
+createLoby.addEventListener('submit', () =>{
     lobyId = Math.floor(10000 + Math.random() * 90000);
     sessionStorage.setItem('lobyId', lobyId)
     sessionStorage.setItem('createLoby', true)   
 })
 
-joinLoby.addEventListener('submit', (e) => {
+joinLoby.addEventListener('submit', () => {
     let joinId = document.getElementById('lobyCode').value
     sessionStorage.setItem('lobyId', joinId)
     sessionStorage.setItem('createLoby', false)   
